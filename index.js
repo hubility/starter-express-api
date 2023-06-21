1018ae32302f8a62c01566b050c30d0521db5b8f
David <hubilityai@gmail.com>
2023-06-21 13:49:42 -0300

    init api”

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.all('/api', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Hola API')
+})
 app.listen(process.env.PORT || 3000)
