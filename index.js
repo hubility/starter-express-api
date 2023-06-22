3abef7d74ee3de5b2494bf7792900f095e78ef8b
David <hubilityai@gmail.com>
2023-06-21 21:41:13 -0300

    update 2

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
 const express = require('express')
+var request = require('request');
 const app = express()
+const axios = require("axios");
 
-async function query(data) {
+async function query(data,res) {
+
+    axios.post("http://34.16.139.61:3000/api/v1/prediction/862b889c-9750-4fa6-8484-fe5aa3435246", {
+      question: data,
+     
+    })
+    .then((response) => {
+      //console.log(response);
+      res.status(200).json({ data: response.data });
+      
+    });
+    //console.log(result);
+/*
     const response = await fetch(
+        
         "http://34.16.139.61:3000/api/v1/prediction/862b889c-9750-4fa6-8484-fe5aa3435246",
         {
             method: "POST",
-            body: data
+            body: {"question": "Hey, how are you?"}
         }
     );
-    const result = await response.json();
+    const result = {"question": "Hey, how are you?"} //await response.json();
     console.log(result);
-    return result;
+    */
+   // return result;
 }
 
+app.all('/', function(req,res) {
+    //modify the url in any way you want
+    var newurl = 'http://google.com/';
+    request("http://34.16.139.61:3000/api/v1/prediction/862b889c-9750-4fa6-8484-fe5aa3435246").pipe(res);
+  });
+/*
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.all('/api/v1/prediction/862b889c-9750-4fa6-8484-fe5aa3435246', (req, res) => {
+*/
+/*
+app.all('/api/v1/prediction/', (req, res) => {
     console.log("Just got a request!")
-   
-    res.send('Hola API')
+    query("eaeee, tudo bem?", res);
+    
+    query("query").then((response) => {
+        res.send(response);
+    });
+    
+    //res.send('Hola API')
 })
+*/
 app.listen(process.env.PORT || 3000)
